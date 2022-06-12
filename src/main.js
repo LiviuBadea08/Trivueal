793878859491f4b814bfe5b65925917b6a9c4d9a
LiviuBadea08 <you@example.com>
2022-06-12 20:44:37 +0200

    added question progress

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
+import "./assets/css/styles.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "./index.css";
 
 createApp(App).use(store).use(router).mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,7 +15,7 @@
 
         <div class="text-right text-gray-800">
           <p class="text-sm leading-3">Score</p>
-          <p class="font-bold">60</p>
+          <p class="font-bold">{{ score }}</p>
         </div>
 
         <!-- timer container -->
@@ -36,15 +36,22 @@
 
         <div class="mt-8">
           <!-- option container -->
+
           <div v-for="(choice, item) in currentQuestion.choices" :key="item">
             <div
-              class="neumorph-1 bg-gray-100 p-2 rounded-lg mb-3"
+              class="neumorph-1 option-default bg-gray-100 p-2 rounded-lg mb-3"
               :ref="optionChosen"
               @click="onOptionClicked(choice, item)"
             >
-              <div class="bg-white rounded-lg font-bold flex p-2">
-                <!-- option id -->
-                <div class="bg-gray-400 p-3 rounded-lg">{{ item }}</div>
+              <div
+                class="bg-blue-700 p-1 transform rotate-45 rounded-md h-10 w-10 text-white font-bold absolute right-0 top-0 shadow-md"
+              >
+                <p class="transform -rotate-45">+10</p>
+              </div>
+
+              <div class="rounded-lg font-bold flex p-2">
+                <!-- option ID -->
+                <div class="p-3 rounded-lg">{{ item }}</div>
 
                 <!-- option name -->
 
@@ -59,7 +66,9 @@
         <!-- progress indicator container-->
         <div class="mt-8 text-center">
           <div class="h-1 w-12 bg-gray-800 rounded-full mx-auto"></div>
-          <p class="font-bold text-color-gray-800">2/10</p>
+          <p class="font-bold text-color-gray-800">
+            {{ questionCounter }}/{{ questions.length }}
+          </p>
         </div>
       </div>
     </div>
@@ -82,7 +91,10 @@ import { ref, onMounted } from "vue";
 export default {
   setup() {
     // data
+    let canClick = true;
+
     let questionCounter = ref(0);
+    let score = ref(0);
     const currentQuestion = ref({
       question: "",
       answer: 1,
@@ -95,17 +107,32 @@ export default {
       {
         question: "Quien chupa mas vergoglios?",
         answer: 1,
-        choices: ["Diegu gallu", "Diegu", "Dieguin", "D"],
+        choices: ["Diegu galluuuuuuuuuuuu", "Diegu", "Dieguin", "D"],
       },
       {
         question: "Quien come mas colitas?",
         answer: 1,
         choices: ["Diegu gallu", "Diegu", "Dieguin", "D"],
       },
+      {
+        question: "es la tercera pregunta?",
+        answer: 1,
+        choices: ["sies", "Diegu", "Dieguin", "D"],
+      },
     ];
 
-    const onQuizStart = () => {
-      currentQuestion.value = questions[questionCounter.value];
+    const loadQuestion = () => {
+      canClick = true;
+      // check lif there are more questions to load
+      if (questions.length > questionCounter.value) {
+        //load question
+        currentQuestion.value = questions[questionCounter.value];
+        console.log("Current question: " + currentQuestion.value);
+        questionCounter.value++;
+      } else {
+        // no more questions
+        console.log("Out of questions");
+      }
     };
 
     // methods/functions
@@ -115,33 +142,53 @@ export default {
         itemsRef.push(element);
       }
     };
+
+    const clearSelected = (divSelected) => {
+      setTimeout(() => {
+        divSelected.classList.remove("option-correct");
+        divSelected.classList.remove("option-wrong");
+        divSelected.classList.add("option-default");
+        loadQuestion();
+      }, 1000);
+    };
+
     const onOptionClicked = (choice, item) => {
-      const divContainer = itemsRef[item];
-      const optionID = item + 1;
-      if (currentQuestion.value.answer == optionID) {
-        console.log("you are correct");
-        divContainer.classList.add("option-correct");
-        divContainer.classList.remove("option-default");
+      if (canClick) {
+        const divContainer = itemsRef[item];
+        const optionID = item + 1;
+        if (currentQuestion.value.answer == optionID) {
+          console.log("you are correct");
+          score.value += 10;
+          divContainer.classList.add("option-correct");
+          divContainer.classList.remove("option-default");
+        } else {
+          console.log("you are wrong");
+          divContainer.classList.add("option-wrong");
+          divContainer.classList.remove("option-default");
+        }
+        canClick = false;
+        // TODO go to next question
+        clearSelected(divContainer);
+        console.log(choice, item);
       } else {
-        console.log("you are wrong");
-        divContainer.classList.add("option-wrong");
-        divContainer.classList.remove("option-default");
+        // cant select option
+        console.log("cant select question");
       }
-      console.log(choice, item);
     };
 
     // lifecycle hooks
 
     onMounted(() => {
-      onQuizStart();
+      loadQuestion();
     });
 
     // return
     return {
       currentQuestion,
       questions,
+      score,
       questionCounter,
-      onQuizStart,
+      loadQuestion,
       onOptionClicked,
       optionChosen,
     };
